d52ce78cb857779f912fcbfef03158d67815caf9
Brandon Guthrie <guthribm@gmail.com>
2021-12-31 11:29:18 -0600

    adds rest of page to mobile layout

---
+++ index.js
@@ -0,0 +1,40 @@
+let monday = document.getElementById("monday");
+let tuesday = document.getElementById("tuesday");
+let wednesday = document.getElementById("wednesday");
+let thursday = document.getElementById("thursday");
+let friday = document.getElementById("friday");
+let saturday = document.getElementById("saturday");
+let sunday = document.getElementById("sunday");
+
+let year = document.getElementById("year");
+
+let today = new Date();
+let currentDay = today.getDay();
+year.textContent = today.getFullYear();
+
+function clearDays() {
+  let days = document.querySelectorAll("tr");
+  for (let day of days) {
+    day.classList.remove("highlighted-day");
+  }
+}
+
+clearDays();
+if (currentDay === 0) {
+  sunday.classList.add("highlighted-day");
+} else if (currentDay === 1) {
+  monday.classList.add("highlighted-day");
+} else if (currentDay === 2) {
+  tuesday.classList.add("highlighted-day");
+} else if (currentDay === 3) {
+  wednesday.classList.add("highlighted-day");
+} else if (currentDay === 4) {
+  thursday.classList.add("highlighted-day");
+} else if (currentDay === 5) {
+  friday.classList.add("highlighted-day");
+} else if (currentDay === 6) {
+  saturday.classList.add("highlighted-day");
+}
+console.log(currentDay);
+
+console.log();
